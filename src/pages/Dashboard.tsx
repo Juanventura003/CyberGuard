c29cd8967347c7881e55c02a1a733ba6829be2d9
dmoney893 <gdaniel998@gmail.com>
2026-09-17 22:01:39 -0400

    Sidebar UI

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,9 @@
+function Dashboard() {
+  return (
+    <div>
+      <h1>Security Dashboard</h1>
+    </div>
+  );
+}
+
+export default Dashboard;
---
+++ src/pages/SecurityChecker.tsx
@@ -0,0 +1,9 @@
+function SecurityChecker() {
+  return (
+    <div>
+      <h1>Security Checker</h1>
+    </div>
+  );
+}
+
+export default SecurityChecker;
